37b5560c02ab294b778456a390ff598dfb3ec4d4
ssangbomb <ssangbomb@gmail.com>
2023-12-19 19:58:34 -0800

    making image list

--- frontend/screens/Appetizer.jsx
+++ frontend/screens/Appetizer.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Image } from 'react-native'; 
 
+const IMAGES = {
+  image1: require('../assets/image/edamame.jpeg'),
+  image2: require('../assets/image/ebi-sunomono.jpeg'),
+  image3: require('../assets/image/yam-fries.jpeg'),
+  image4: require('../assets/image/tuna-tataki.jpeg'),
+  image5: require('../assets/image/salmon-tataki.jpeg'),
+}
+
 const Appetizer = () => {
+  const [ images, setImages ] = usestate([
+    { id: '1', image: IMAGES.image1 },
+    { id: '2', image: IMAGES.image2 },
+    { id: '3', image: IMAGES.image3 },
+    { id: '4', image: IMAGES.image4 },
+    { id: '5', image: IMAGES.image5 },
+  ])
   return (
-    <View>
-       <Image 
-        source={require("../assets/image/salmon-tataki.jpeg")}
-        style={{width: 100, height: 100}}
-        contentFit='cover'
-        transition={1000}
-       />
+    <View style={{}}>
+
          <Text>Salmon Tataki</Text>
     </View>
   )
